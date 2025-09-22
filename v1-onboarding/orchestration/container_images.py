0e5a55eba74f84ab053ce8f61877429aa43d5ca3
davidmirror-ops <david.espejo@union.ai>
2025-09-22 14:04:25 -0500

    First commit with secrets
    Signed-off-by: davidmirror-ops <david.espejo@union.ai>

---
+++ v1-onboarding/orchestration/container_images.py
@@ -0,0 +1,19 @@
+from union import ImageSpec
+
+container_image = ImageSpec(
+    name="example-image",
+    builder="union",
+    requirements="./requirements.txt",
+    builder_options={
+        "imagepull_secret_name":"ghcr-pull-creds-02",
+    }
+)
+
+app_image = ImageSpec(
+    name="union-serve-keras-fastapi",
+    packages=["union-runtime>=0.1.10", "tensorflow", "fastapi[standard]", "pillow"],
+    builder="union",
+    builder_options={
+        "imagepull_secret_name":"ghcr-pull-creds-02",
+    } 
+)
